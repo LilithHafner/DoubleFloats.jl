fd5e5829d20790797d2eb189f4b6d84e5603eeb9
JeffreySarnoff <jeffrey.sarnoff@gmail.com>
2022-05-30 16:09:07 -0400

    cleanup

--- test/morearith.jl
+++ test/morearith.jl
@@ -17,8 +17,8 @@ dinf, finf = DT(Inf), FT(Inf)
 d0, f0 = zero(DT), zero(FT)
 d1, f1 = one(DT), one(FT)
 
-dmin, fmin = floatmin(DT), floatmin(FT)
-dmax, fmin = floatmax(DT), floatmax(FT)
+dmin, fmin = floatmin(DT), HI(floatmin(DT))
+dmax, fmax = floatmax(DT), HI(floatmax(DT))
 dmin₊, fmin₊ = dmin * (1025/1024), fmin * (1025/1024)
 dmax₋, fmax₋ = dmax * (1023/1024), fmax * (1023/1024)
 
@@ -31,15 +31,22 @@ cbrt2_dmax, cbrt2_fmax = cbrt_dmax^2, cbrt_fmax^2
 
 sqrt_dmin₊, sqrt_fmin₊ = sqrt(dmin₊), sqrt(fmin₊)
 sqrt_dmax₋, sqrt_fmax₋ = sqrt(dmax₋), sqrt(fmax₋)
-cbrt_dmax₋, cbrt_fmax₋ = cbrt(dmax₋), cbrt(fmax₋)
-sqrt_dmax₋, sqrt_fmax₋ = sqrt(dmax₋), sqrt(fmax₋)
+cbrt_dmin₊, cbrt_fmin₊ = cbrt(dmin₊), cbrt(fmin₊)
+cbrt_dmax₋, cbrt_fmax₋ = sqrt(dmax₋), sqrt(fmax₋)
 cbrt2_dmin₊, cbrt2_fmin₊ = cbrt_dmin₊^2, cbrt_fmin₊^2 
 cbrt2_dmax₋, cbrt2_fmax₋ = cbrt_dmax₋^2, cbrt_fmax₋^2
 
-dvals = (dnan, dinf, d0, d1, dmin, dmax, dmin₊, dmax₋, sqrt_dmin, sqrt_dmax, sqrt_dmin₊, sqrt_dmax₋, 
-         cbrt_dmin, cbrt_dmax, cbrt_dmin₊, cbrt_dmax₋, cbrt2_dmin, cbrt2_dmax, cbrt2_dmin₊, cbrt2_dmax₋,)
-fvals = (fnan, finf, f0, f1, dmin, fmax, fmin₊, fmax₋, sqrt_fmin, sqrt_fmax, sqrt_fmin₊, sqrt_fmax₋, 
-         cbrt_fmin, cbrt_fmax, cbrt_fmin₊, cbrt_fmax₋, cbrt2_fmin, cbrt2_fmax, cbrt2_fmin₊, cbrt2_fmax₋,)
+dvals = (dnan, dinf, d0, d1, 
+        dmin, dmax, dmin₊, dmax₋, 
+        sqrt_dmin, sqrt_dmax, sqrt_dmin₊, sqrt_dmax₋, 
+        cbrt_dmin, cbrt_dmax, cbrt_dmin₊, cbrt_dmax₋,
+        cbrt2_dmin, cbrt2_dmax, cbrt2_dmin₊, cbrt2_dmax₋,)
+
+fvals = (fnan, finf, f0, f1, 
+        dmin, fmax, fmin₊, fmax₋, 
+        sqrt_fmin, sqrt_fmax, sqrt_fmin₊, sqrt_fmax₋, 
+        cbrt_fmin, cbrt_fmax, cbrt_fmin₊, cbrt_fmax₋,
+        cbrt2_fmin, cbrt2_fmax, cbrt2_fmin₊, cbrt2_fmax₋,)
 
 dfvals = zip(dvals, fvals)
 
